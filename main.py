11c4fba8f444ade1ba9e604b7d0717882bab1198
Antonio Urdaneta <123696971+aaurdanet@users.noreply.github.com>
2024-04-04 19:48:21 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas
+
+# Create a dictionary from csv
+
+nato = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter: row.code for index, row in nato.iterrows()}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+input_word = input("Enter a word: ").upper()
+char_list = [char for char in input_word]
+
+output = [nato_dict[key] if key in nato_dict else None for key in char_list]
+
+print(output)
